ad8f7c4e7a549b507901cc7edf360cc0040b5f2c
Wersiy Odilous <wersiy.odilous@gmail.com>
2023-11-14 17:14:31 +0100

    re-organize my local repo

--- install_role/files/script_install.sh
+++
@@ -1,3 +0,0 @@
-        #!/bin/bash
-        rm -rf /var/www/html/* 
-        cp -rf /home/ec2-user/httpd/medlife-master/* /var/www/html/
--- uninstall_role/files/script_uninstall.sh
+++
@@ -1,4 +0,0 @@
-        #!/bin/bash
-        rm -rf /var/www/html/*
-        rm -rf /home/ec2-user/httpd
-	rm -rf /tmp/httpd/*
